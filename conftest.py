00b713a09bdeee947c38161a1eaaa00d2a34a3bd
Hiren Alphabin <hiren.alphabin@gmail.com>
2025-03-31 14:47:17 +0530

    Updated conftest.py file

--- conftest.py
+++ conftest.py
@@ -5,6 +5,9 @@ from selenium import webdriver
 def setup_teardown():
     """Setup and teardown WebDriver session."""
     driver = webdriver.Chrome()  # Update if using another browser
+    webdriver.options.add_argument("--headless")  # Run in headless mode
+    webdriver.options.add_argument("--no-sandbox")
+    webdriver.options.add_argument("--disable-dev-shm-usage")
     driver.maximize_window()
     driver.get("https://mypustak.com")  # Replace with actual URL
     yield driver
